824f6b47b8f3b58e1b8359bd1097cc94df66518d
Xiong <xiongz@andrew.cmu.edu>
2021-02-19 18:26:11 -0800

    linted

--- src/components/OperatingHoursTab/OperationHoursGroup.jsx
+++ src/components/OperatingHoursTab/OperationHoursGroup.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Row, Col, Tabs} from 'antd';
+import { Row, Tabs } from 'antd';
 import TimeTable from './TimeTable';
 
 const { TabPane } = Tabs;
 
 const OperationHoursGroup = () => {
-    return (
-        <div>
-            <Row>Operation Hours</Row>
-            <Row>
-                <Tabs defaultActiveKey="1">
-                    <TabPane tab="Dine-in" key="1">
-                        <TimeTable tabName={"Dine-in"}/>
-                    </TabPane>
-                    <TabPane tab="Delivery" key="2">
-                        <TimeTable tabName={"Delivery"}/>
-                    </TabPane>
-                </Tabs>
-            </Row>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Row>Operation Hours</Row>
+      <Row>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="Dine-in" key="1">
+            <TimeTable tabName={'Dine-in'} />
+          </TabPane>
+          <TabPane tab="Delivery" key="2">
+            <TimeTable tabName={'Delivery'} />
+          </TabPane>
+        </Tabs>
+      </Row>
+    </div>
+  );
+};
 
 export default OperationHoursGroup;
--- src/components/OperatingHoursTab/SpecialHoursGroup.jsx
+++ src/components/OperatingHoursTab/SpecialHoursGroup.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Row, Col, Tabs, Divider} from 'antd';
+import { Row, Tabs, Divider } from 'antd';
 import SpecialTimeTable from './SpecialTimeTable';
 
 const { TabPane } = Tabs;
 
 const SpecialHoursGroup = () => {
-    return (
-        <div>
-            <Row>Special Hours</Row>
-            <Row>
-                <Tabs defaultActiveKey="1">
-                    <TabPane tab="Dine-in" key="1">
-                        <SpecialTimeTable tabName={"Dine-in"}/>
-                    </TabPane>
-                    <TabPane tab="Delivery" key="2">
-                        <SpecialTimeTable tabName={"Delivery"}/>
-                    </TabPane>
-                </Tabs>
-            </Row>
-            <Row>
-                <Divider />
-            </Row>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Row>Special Hours</Row>
+      <Row>
+        <Tabs defaultActiveKey="1">
+          <TabPane tab="Dine-in" key="1">
+            <SpecialTimeTable tabName={'Dine-in'} />
+          </TabPane>
+          <TabPane tab="Delivery" key="2">
+            <SpecialTimeTable tabName={'Delivery'} />
+          </TabPane>
+        </Tabs>
+      </Row>
+      <Row>
+        <Divider />
+      </Row>
+    </div>
+  );
+};
 
 export default SpecialHoursGroup;
